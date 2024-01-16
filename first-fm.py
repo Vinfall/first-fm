b9b5f0a5fcab4282918a8c441fd00d45b255fd25
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-01-16 19:03:41 +0800

    Lint file

--- first-fm.py
+++ first-fm.py
@@ -30,16 +30,17 @@ _TO_IGNORE = [
     "Pure Music",
 ]
 
+
 def load_secrets():
     # Load screct credentials from local file
     secrets_file = ".env.local"
     if os.path.isfile(secrets_file):
-        with open(secrets_file, 'r') as f:
+        with open(secrets_file, "r") as f:
             lines = f.readlines()
             doc = {}
             for line in lines:
-                if '=' in line:
-                    key, value = line.split(' = ')
+                if "=" in line:
+                    key, value = line.split(" = ")
                     doc[key.strip()] = value.strip().strip('"')
     else:
         # Load screct credentials from environment variables
@@ -47,24 +48,23 @@ def load_secrets():
             "API_KEY": os.environ.get("PYLAST_API_KEY", "").strip(),
             "API_SECRET": os.environ.get("PYLAST_API_SECRET", "").strip(),
             "username": os.environ.get("PYLAST_USERNAME", "").strip(),
-            "password": os.environ.get("PYLAST_PASSWORD", "").strip()
+            "password": os.environ.get("PYLAST_PASSWORD", "").strip(),
         }
         if not all(doc.values()):
             raise EnvironmentError("Missing environment variables, check .env.local")
     return doc
 
+
 def read_tracks_file():
     tracks = []
-    with open(_TRACK_FILE, 'r', encoding='utf-8') as file:
+    with open(_TRACK_FILE, "r", encoding="utf-8") as file:
         for line in file:
             # Split using only the first occurrence of ' - '
-            artist, title = line.strip().split(' - ', 1)
-            track_info = {
-                "artist": artist,
-                "title": title
-            }
+            artist, title = line.strip().split(" - ", 1)
+            track_info = {"artist": artist, "title": title}
             tracks.append(track_info)
     return tracks
+
 
 secrets = load_secrets()
 
@@ -89,4 +89,4 @@ for track in tracks:
     # Ignore certain type of tracks
     if artist != "ID" and not any(keyword in title for keyword in _TO_IGNORE):
         last_api_call = network.scrobble(artist, title, int(time.time()))
-    print ("Scrobbled: " + artist + " - " + title)
+    print("Scrobbled: " + artist + " - " + title)
